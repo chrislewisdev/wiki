aafac7752a2167ced5ea20935c50d8041669e937
Chris Lewis <chris.lewis@campaignmonitor.com>
2021-12-24 17:24:59 +1100

    Very basic html build generation

--- main.go
+++ main.go
@@ -1,17 +1,56 @@
 package main
 
 import (
-	"fmt"
 	"os"
+    "strings"
+    "github.com/gomarkdown/markdown"
 )
 
 func main() {
-	contents, err := os.ReadDir("content")
+    header := `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    `
+    footer := "</body></html>"
+
+    contentDirectory := "content"
+	contents, err := os.ReadDir(contentDirectory)
+    if err != nil {
+        panic(err)
+    }
+
+    err = os.Mkdir("build", 0755)
     if err != nil {
         panic(err)
     }
 
     for _, entry := range contents {
-        fmt.Println(entry.Name(), entry.IsDir())
+        mdName := entry.Name()
+        htmlName := strings.Replace(mdName, ".md", ".html", -1)
+        md, err := os.ReadFile(contentDirectory + "/" + mdName)
+        if err != nil {
+            panic(err)
+        }
+
+        body := markdown.ToHTML(md, nil, nil)
+        html := header + "<h1>" + mdName + "</h1>\n" + string(body) + footer
+
+        file, err := os.Create("build/" + htmlName)
+        if err != nil {
+            panic(err)
+        }
+        defer file.Close()
+
+        _, err = file.WriteString(html)
+        if err != nil {
+            panic(err)
+        }
     }
 }
